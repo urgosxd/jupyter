57a91fad077d0866517c3a696aa6afc7669f3372
urgosxd <60458146+urgosxd@users.noreply.github.com>
2020-12-27 20:57:43 -0500

    asd

--- fun.py
+++
@@ -1,4 +0,0 @@
-!matplotlib inline
-import matplotlib.pyplot as plt
-plt.plot([[0,0],[1,1]],linewidth=4,label='random diagonal')
-plt.show()
--- funciones.py
+++ funciones.py
@@ -1,2 +1,10 @@
-def sum(a,b):
-    return a+b
+from sympy import *
+from sympy.calculus.util import continuous_domain, function_range
+from sympy.plotting import plot
+
+
+def Funciones(variable, expresion):
+    dominio = continuous_domain(expresion, variable, S.Reals)
+    rango = function_range(expresion, variable, dominio)
+    p1 = plot(expresion, show=False)
+    return dominio, rango, p1
